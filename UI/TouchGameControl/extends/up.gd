c5a551df5072a112c9468850aeaae1b203087d97
PuKYC <15068231345@163.com>
2025-04-06 21:17:39 +0800

    修改gui

--- UI/TouchGameControl/extends/up.gd
+++
@@ -1,8 +0,0 @@
-extends TouchControlBase
-
-
-func on_control_started(_event: InputEventScreenTouch):
-	Player.set_verticale_up_input(1.0)
-	
-func on_control_ended():
-	Player.set_verticale_up_input(0.0)
---
+++ UI/TouchGameControl/rotation.gd
@@ -0,0 +1,55 @@
+extends TouchControlBase
+
+@export var rotation_sensitivity := 0.002
+@export var vertical_clamp := Vector2(deg_to_rad(-80), deg_to_rad(80))
+
+var camera_start_rotation = Vector3.ZERO
+var touch_start_position = Vector2.ZERO
+var position_delta = Vector2.ZERO
+
+func on_control_started(_event: InputEventScreenTouch):
+		camera_start_rotation = Player.get_camera_rotation()
+		touch_start_position = _event.position
+
+func on_control_updated(event: InputEventScreenDrag):
+			
+		position_delta = event.position - touch_start_position
+		Player.set_see_rotation(Vector3(
+				clampf(camera_start_rotation.x + position_delta.y * rotation_sensitivity, 
+						vertical_clamp.x, vertical_clamp.y),
+				camera_start_rotation.y + position_delta.x * rotation_sensitivity,
+				0
+		))
+
+func on_control_ended():
+		camera_start_rotation = Vector3.ZERO
+		touch_start_position = Vector3.ZERO
+		
+func is_point_in_control_area(pos: Vector2) -> bool:
+	return control_point_in_area(self, pos) and is_point_in_node(pos)
+	
+func is_point_in_node(pos) -> bool:
+	for child in get_children():
+		if control_point_in_area(child, pos):
+			return false
+	return true
+
+
+func _on_top_button_down() -> void:
+	Player.set_verticale_up_input(1.0)
+	pass # Replace with function body.
+
+
+func _on_top_button_up() -> void:
+	Player.set_verticale_up_input(0.0)
+	pass # Replace with function body.
+
+
+func _on_down_button_down() -> void:
+	Player.set_verticale_down_input(1.0)
+	pass # Replace with function body.
+
+
+func _on_down_button_up() -> void:
+	Player.set_verticale_down_input(0.0)
+	pass # Replace with function body.
